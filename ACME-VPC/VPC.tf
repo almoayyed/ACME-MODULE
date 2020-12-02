17ae0c686683357a8ece0ca3d380c55fdfb0ae8b
Ubuntu <ubuntu@ip-10-0-2-247.eu-west-1.compute.internal>
2020-12-02 04:52:50 +0000

    updated

--- ACME-VPC/VPC.tf
+++ ACME-VPC/VPC.tf
@@ -1,50 +1,6 @@
-// ACME VPC Resource for Module
-
-provider "aws" {
-  region = "eu-west-1"
-    
-}
-
 ###################
 # RESOURCE
 ###################
-
-## Transit Gateway deployed only in NATWORK Account . Total 3 TG need to be deployed in network account. and rest account transit gateway attachment will be done manually
-/*
-resource "aws_ec2_transit_gateway" "test-tgw" {
-  description                     = "Transit Gateway creating for cross account network enable"
-  default_route_table_association = "disable"
-  default_route_table_propagation = "disable"
-  tags                            = {
-    Name               = "${var.Region}-${var.env}-TG-${var.service}"
-    Environment        = "${var.Environment}"
-    Owner              = "${var.Owner}"
-    CostCenter         = "${var.CostCenter}"
-    Description        = "${var.Environment} Environment DHCP Option set"
-}
-}
-
-## DHCP to be deployed in all account except PROD & DEV SHARED Account. Because DNS1 AND DN2 ip will be available after deployment of AD in shared account.
-resource "aws_vpc_dhcp_options" "dhcp" {
-  domain_name          = "${var.domain_name}" #"acme.local"
-  domain_name_servers  = ["${var.dns1}", "${var.dns2}"]
-  #ntp_servers          = ["127.0.0.1"]
-
-  tags = {
-    Name               = "${var.Region}-${var.env}-DHCP-${var.service}"
-    Environment        = "${var.Environment}"
-    Owner              = "${var.Owner}"
-    CostCenter         = "${var.CostCenter}"
-    Description        = "${var.Environment} Environment DHCP Option set"
-  }
-}
-
-
-resource "aws_vpc_dhcp_options_association" "dns_resolver" {
-  vpc_id          = "${aws_vpc.main_vpc.id}"
-  dhcp_options_id = "${aws_vpc_dhcp_options.dhcp.id}"
-}
-*/
 
 
 ## VPC Flow Logs deployed of each account VPC
@@ -56,19 +12,18 @@ resource "aws_flow_log" "vpcflow" {
   vpc_id          = "${aws_vpc.main_vpc.id}"
 }
 
-
-
 resource "aws_cloudwatch_log_group" "vpcflow" {
   #name = "${var.Region}-${var.env}-VPC-${var.service}-${var.vpcrange}-vpcflowlog"
-  retention_in_days = "90"
-  name_prefix = "${var.Region}-${var.env}-VPC-${var.service}-${var.vpcrange}-CW"
+  retention_in_days = "180"
+  name_prefix       = "${var.Region}-${var.env}-VPC-${var.service}-${var.vpcrange}-CW"
+
   tags = {
-    Name               = "${var.Region}-${var.env}-CW-${var.service}"
-    Environment        = "${var.Environment}"
-    Owner              = "${var.Owner}"
-    CostCenter         = "${var.CostCenter}"
-    Description        = "${var.Environment} Environment VPC ${var.vpcrange}"
-}
+    Name        = "${var.Region}-${var.env}-CW-${var.service}"
+    Environment = "${var.Environment}"
+    Owner       = "${var.Owner}"
+    
+    Description = "${var.Environment} Environment VPC ${var.vpcrange}"
+  }
 }
 
 resource "aws_iam_role" "vpcflow" {
@@ -90,13 +45,13 @@ resource "aws_iam_role" "vpcflow" {
 }
 EOF
 
-tags = {
-    Name               = "${var.Region}-${var.env}-IAM-${var.service}"
-    Environment        = "${var.Environment}"
-    Owner              = "${var.Owner}"
-    CostCenter         = "${var.CostCenter}"
-    Description        = "${var.Environment} Environment VPC ${var.vpcrange}"
-}
+  tags = {
+    Name        = "${var.Region}-${var.env}-IAM-${var.service}"
+    Environment = "${var.Environment}"
+    Owner       = "${var.Owner}"
+    
+    Description = "${var.Environment} Environment VPC ${var.vpcrange}"
+  }
 }
 
 resource "aws_iam_role_policy" "vpcflow" {
@@ -123,22 +78,20 @@ resource "aws_iam_role_policy" "vpcflow" {
 EOF
 }
 
-
-## VPC
+####################################### VPC ##########################################################3
 resource "aws_vpc" "main_vpc" {
   cidr_block           = "${var.vpc_cidr}"
   enable_dns_hostnames = "true"
   enable_dns_support   = "true"
 
   tags = {
-    Name               = "${var.Region}-${var.env}-VPC-${var.service}-${var.vpcrange}"
-    Environment        = "${var.Environment}"
-    Owner              = "${var.Owner}"
-    CostCenter         = "${var.CostCenter}"
-    Description        = "${var.Environment} Environment VPC ${var.vpcrange}"
+    Name        = "${var.Region}-${var.env}-VPC-${var.service}-${var.vpcrange}"
+    Environment = "${var.Environment}"
+    Owner       = "${var.Owner}"
+    
+    Description = "${var.Environment} Environment VPC ${var.vpcrange}"
   }
 }
-
 
 ###################
 # Public subnets  #
@@ -151,11 +104,11 @@ resource "aws_subnet" "public" {
   count             = "${length(var.public_subnet_cidr)}"
 
   tags = {
-    Name               = "${var.Region}-${var.env}-SN-${var.service}-PB${element(var.az-tag, count.index)}"
-    Environment        = "${var.Environment}"
-    Owner              = "${var.Owner}"
-    CostCenter         = "${var.CostCenter}"
-    Description        = "${var.Environment} Environment Public Subnet"
+    Name        = "${var.Region}-${var.env}-SN-LB${element(var.az-tag, count.index)}"
+    Environment = "${var.Environment}"
+    Owner       = "${var.Owner}"
+    
+    Description = "${var.Environment} Environment Public Subnet"
   }
 
   #map_public_ip_on_launch = true
@@ -163,15 +116,15 @@ resource "aws_subnet" "public" {
 
 resource "aws_route_table" "public" {
   vpc_id = "${aws_vpc.main_vpc.id}"
- #count  = "${length(var.public_subnet_cidr)}"
 
+  #count  = "${length(var.public_subnet_cidr)}"
 
   tags = {
-    Name               = "${var.Region}-${var.env}-RT-${var.service}-PB${element(var.az-tag, count.index)}"
-    Environment        = "${var.Environment}"
-    Owner              = "${var.Owner}"
-    CostCenter         = "${var.CostCenter}"
-    Description        = "${var.Environment} Environment Public Subnet RouteTable"
+    Name        = "${var.Region}-${var.env}-RT-LB${element(var.az-tag, count.index)}"
+    Environment = "${var.Environment}"
+    Owner       = "${var.Owner}"
+    
+    Description = "${var.Environment} Environment Public Subnet RouteTable"
   }
 }
 
@@ -180,7 +133,6 @@ resource "aws_route_table_association" "public" {
   subnet_id      = "${element(aws_subnet.public.*.id, count.index)}"
   route_table_id = "${aws_route_table.public.id}"
 }
-
 
 ###################
 # APP subnets #
@@ -193,20 +145,21 @@ resource "aws_subnet" "private" {
   count             = "${length(var.private_subnet_cidr)}"
 
   tags = {
-    Name               = "${var.Region}-${var.env}-SN-${var.service}-AP${element(var.az-tag, count.index)}"
-    Environment        = "${var.Environment}"
-    Owner              = "${var.Owner}"
-    CostCenter         = "${var.CostCenter}"
-    Description        = "${var.Environment} Environment Application Subnet"
-}
+    Name        = "${var.Region}-${var.env}-SN-AP${element(var.az-tag, count.index)}"
+    Environment = "${var.Environment}"
+    Owner       = "${var.Owner}"
+    
+    Description = "${var.Environment} Environment Application Subnet"
+  }
 }
 
+/*
 resource "aws_route_table" "private" {
   vpc_id = "${aws_vpc.main_vpc.id}"
   #count  = "${length(var.private_subnet_cidr)}"
 
   tags = {
-    Name               = "${var.Region}-${var.env}-RT-${var.service}-AP${element(var.az-tag, count.index)}"
+    Name               = "${var.Region}-${var.env}-RT-AP${element(var.az-tag, count.index)}"
     Environment        = "${var.Environment}"
     Owner              = "${var.Owner}"
     CostCenter         = "${var.CostCenter}"
@@ -222,7 +175,6 @@ resource "aws_route_table_association" "private" {
 }
 */
 
-
 ##################################
 # SERVICE-SUBNET
 ##################################
@@ -234,27 +186,28 @@ resource "aws_subnet" "service" {
   count             = "${length(var.service_subnet_cidr)}"
 
   tags = {
-    Name               = "${var.Region}-${var.env}-SN-${var.service}-SR${element(var.az-tag, count.index)}"
-    Environment        = "${var.Environment}"
-    Owner              = "${var.Owner}"
-    CostCenter         = "${var.CostCenter}"
-    Description        = "${var.Environment} Environment Service Subnet"
-}
+    Name        = "${var.Region}-${var.env}-SN-SR${element(var.az-tag, count.index)}"
+    Environment = "${var.Environment}"
+    Owner       = "${var.Owner}"
+    
+    Description = "${var.Environment} Environment Service Subnet"
+  }
 }
 
+/*
 resource "aws_route_table" "service" {
   vpc_id = "${aws_vpc.main_vpc.id}"
   #count  = "${length(var.private_subnet_cidr)}"
 
   tags = {
-    Name               = "${var.Region}-${var.env}-RT-${var.service}-SR${element(var.az-tag, count.index)}"
+    Name               = "${var.Region}-${var.env}-RT-SR${element(var.az-tag, count.index)}"
     Environment        = "${var.Environment}"
     Owner              = "${var.Owner}"
     CostCenter         = "${var.CostCenter}"
     Description        = "${var.Environment} Environment Service Subnet RouteTable"
 }
 }
-/*
+
 resource "aws_route_table_association" "service" {
   count          = "${length(var.service_subnet_cidr)}"
   subnet_id      = "${element(aws_subnet.service.*.id, count.index)}"
@@ -274,27 +227,28 @@ resource "aws_subnet" "db" {
   count             = "${length(var.db_subnet_cidr)}"
 
   tags = {
-    Name               = "${var.Region}-${var.env}-SN-${var.service}-DB${element(var.az-tag, count.index)}"
-    Environment        = "${var.Environment}"
-    Owner              = "${var.Owner}"
-    CostCenter         = "${var.CostCenter}"
-    Description        = "${var.Environment} Environment Database Subnet"
-}
+    Name        = "${var.Region}-${var.env}-SN-DB${element(var.az-tag, count.index)}"
+    Environment = "${var.Environment}"
+    Owner       = "${var.Owner}"
+    
+    Description = "${var.Environment} Environment Database Subnet"
+  }
 }
 
+/*
 resource "aws_route_table" "db" {
   vpc_id = "${aws_vpc.main_vpc.id}"
   #count  = "${length(var.private_subnet_cidr)}"
 
   tags = {
-    Name               = "${var.Region}-${var.env}-RT-${var.service}-DB${element(var.az-tag, count.index)}"
+    Name               = "${var.Region}-${var.env}-RT-DB${element(var.az-tag, count.index)}"
     Environment        = "${var.Environment}"
     Owner              = "${var.Owner}"
     CostCenter         = "${var.CostCenter}"
     Description        = "${var.Environment} Environment Database Subnet RouteTable"
   }
 }
-/*
+
 resource "aws_route_table_association" "db" {
   count          = "${length(var.db_subnet_cidr)}"
   subnet_id      = "${element(aws_subnet.db.*.id, count.index)}"
@@ -313,149 +267,56 @@ resource "aws_subnet" "tg" {
   count             = "${length(var.tg_subnet_cidr)}"
 
   tags = {
-    Name               = "${var.Region}-${var.env}-SN-${var.service}-TG${element(var.az-tag, count.index)}"
-    Environment        = "${var.Environment}"
-    Owner              = "${var.Owner}"
-    CostCenter         = "${var.CostCenter}"
-    Description        = "${var.Environment} Environment Transit Gateway Subnet"
-}
+    Name        = "${var.Region}-${var.env}-SN-TG${element(var.az-tag, count.index)}"
+    Environment = "${var.Environment}"
+    Owner       = "${var.Owner}"
+    
+    Description = "${var.Environment} Environment Transit Gateway Subnet"
+  }
 }
 
 resource "aws_route_table" "tg" {
   vpc_id = "${aws_vpc.main_vpc.id}"
+
   #count  = "${length(var.private_subnet_cidr)}"
 
   tags = {
-    Name               = "${var.Region}-${var.env}-RT-${var.service}-TG${element(var.az-tag, count.index)}"
-    Environment        = "${var.Environment}"
-    Owner              = "${var.Owner}"
-    CostCenter         = "${var.CostCenter}"
-    Description        = "${var.Environment} Environment Transit Gateway RouteTable"
-}
+    Name        = "${var.Region}-${var.env}-RT-TG${element(var.az-tag, count.index)}"
+    Environment = "${var.Environment}"
+    Owner       = "${var.Owner}"
+    
+    Description = "${var.Environment} Environment Transit Gateway RouteTable"
+  }
 }
 
 resource "aws_route_table_association" "tg" {
-  count          = "${length(var.tg_subnet_cidr)}"
-  subnet_id      = "${element(aws_subnet.tg.*.id, count.index)}"
+  count     = "${length(var.tg_subnet_cidr)}"
+  subnet_id = "${element(aws_subnet.tg.*.id, count.index)}"
+
   #route_table_id = "${element(aws_route_table.private.*.id, count.index)}"
   route_table_id = "${aws_route_table.tg.id}"
 }
 
-
-
 resource "aws_route_table_association" "db" {
-  count          = "${length(var.db_subnet_cidr)}"
-  subnet_id      = "${element(aws_subnet.db.*.id, count.index)}"
+  count     = "${length(var.db_subnet_cidr)}"
+  subnet_id = "${element(aws_subnet.db.*.id, count.index)}"
+
   #route_table_id = "${element(aws_route_table.private.*.id, count.index)}"
   route_table_id = "${aws_route_table.tg.id}"
 }
 
 resource "aws_route_table_association" "service" {
-  count          = "${length(var.service_subnet_cidr)}"
-  subnet_id      = "${element(aws_subnet.service.*.id, count.index)}"
+  count     = "${length(var.service_subnet_cidr)}"
+  subnet_id = "${element(aws_subnet.service.*.id, count.index)}"
+
   #route_table_id = "${element(aws_route_table.private.*.id, count.index)}"
   route_table_id = "${aws_route_table.tg.id}"
 }
 
 resource "aws_route_table_association" "private" {
-  count          = "${length(var.private_subnet_cidr)}"
-  subnet_id      = "${element(aws_subnet.private.*.id, count.index)}"
+  count     = "${length(var.private_subnet_cidr)}"
+  subnet_id = "${element(aws_subnet.private.*.id, count.index)}"
+
   #route_table_id = "${element(aws_route_table.private.*.id, count.index)}"
   route_table_id = "${aws_route_table.tg.id}"
 }
-
-
-##### VARIABLE
-
-######################
-# TAGS
-
-variable "Region" {
-  default = "ME"
-}
-
-variable "env" {
-  default = "P"
-}
-
-variable "service" {
-  default = "NS"
-}
-
-
-variable "Environment" {
-  default = "PROD"
-}
-
-variable "Owner" {
-  default = "ACME"
-}
-
-variable "CostCenter" {
-  default = "Billing"
-}
-
-
-###########################################
-#VPC  Variables
-############################################
-
-variable "availability_zones" {
-  default     = ["eu-west-1a", "eu-west-1b"]
-  description = "The availability zones the we should create subnets in, launch instances in, and configure for ELB and ASG"
-}
-
-variable "az-tag" {
-  default = ["01", "02"]
-}
-
-variable "vpc_cidr" {
-  default     = "10.0.0.0/16"
-  description = "The range of IP addresses that we use in this VPC"
-}
-
-variable "public_subnet_cidr" {
-  default     = ["10.0.9.0/24"]
-  description = "CIDR blocks for public subnets. Number of entries must match 'availability_zones'."
-}
-
-variable "private_subnet_cidr" {
-  default     = ["10.0.5.0/24"]
-  description = "CIDR blocks for private subnets. Number of entries must match 'availability_zones'."
-}
-
-variable "service_subnet_cidr" {
-  default     = ["10.0.6.0/24"]
-  description = "CIDR blocks for public subnets. Number of entries must match 'availability_zones'."
-}
-
-variable "tg_subnet_cidr" {
-  default     = ["10.0.2.0/24"]
-  description = "CIDR blocks for public subnets. Number of entries must match 'availability_zones'."
-}
-
-variable "db_subnet_cidr" {
-    default = ["10.0.1.0/24"]
-    description = "CIDR blocks for public subnets. Number of entries must match 'availability_zones'."
-}
-
-# variable "domain_name" {
-#   default = ""
-# }
-
-# variable "dns1" {
-#   default = ""
-# }
-
-# variable "dns2" {
-#   default = ""
-# }
-
-variable "vpcrange" {
-    default = "000"
-}
-
-
-# output "vpc_id" {
-#   value = "${aws_vpc.main_vpc.id}"
-# }
