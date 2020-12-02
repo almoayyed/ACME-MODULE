aad3ddb9f40cd4a0971d82b62ff9198521edc1a5
Ubuntu <ubuntu@ip-10-0-2-247.eu-west-1.compute.internal>
2020-12-02 03:33:22 +0000

    updated module

--- AC-CONFIG-CT/main.tf
+++ AC-CONFIG-CT/main.tf
@@ -1,3 +1,5 @@
+data "aws_caller_identity" "current" {}
+
 resource "aws_config_configuration_recorder" "ConfigurationRecorder" {
   role_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-controltower-ConfigRecorderRole"
 
