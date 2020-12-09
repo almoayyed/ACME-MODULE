31d9aad93e173d0622b1f9417eacf00f36550ddf
Ankit Upadhyay <38682280+ankitosh@users.noreply.github.com>
2020-12-09 10:13:51 +0300

    update the variable

---
+++ AC-S3BLOCKPUBLIC-CT/variable.tf
@@ -0,0 +1,20 @@
+
+
+variable "block_public_acls" {
+  default = "true"
+}
+
+
+variable "ignore_public_acls" {
+  default = "true"
+}
+
+
+variable "block_public_policy" {
+  default = "true"
+}
+
+
+variable "restrict_public_buckets" {
+  default = "true"
+}
