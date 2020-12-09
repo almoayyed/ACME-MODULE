c057605d1e618492816ac0862ffa9af43cc50d8c
Ankit Upadhyay <38682280+ankitosh@users.noreply.github.com>
2020-12-09 09:35:48 +0300

    Added as variable

--- AC-S3BLOCKPUBLIC-CT/main.tf
+++ AC-S3BLOCKPUBLIC-CT/main.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_account_public_access_block" "BlockPublicAccess" {
-  block_public_acls = "true"
-  ignore_public_acls = "true"
-  block_public_policy = "true"
-  restrict_public_buckets = "true"
+  block_public_acls = "${var.block_public_acls}"
+  ignore_public_acls = "${var.ignore_public_acls}"
+  block_public_policy = "${var.block_public_policy}"
+  restrict_public_buckets = "${var.restrict_public_buckets}"
 }
