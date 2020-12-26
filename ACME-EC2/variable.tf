580c2456cef243725e6e11d370d37c083c1efe6e
Ubuntu <ubuntu@ip-10-0-2-247.eu-west-1.compute.internal>
2020-12-26 10:43:57 +0000

    updated code on ec2 module

--- ACME-EC2/variable.tf
+++ ACME-EC2/variable.tf
@@ -32,17 +32,24 @@ variable "APPVpcName" {
     default = ""
 }
 
-variable "ami_pattern" {
+variable "ami_pattern" { # https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/finding-an-ami.html
   default = {
-    "awsami"  = "Amazon Linux AMI*"
-    "linux"   = "RHEL-7.5_HVM_GA-*"
-    "windows" = "Windows_Server-2016-English-Full-Base*"
-    "ubuntu"  = "ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server*"
+    "awsami"  = "amzn2-ami-hvm-2.0*" # 137112412989
+    "redhat7"   = "RHEL-7.9_HVM_GA-*" # Red Hat Enterprise Linux 7.0_HVM_GA # 309956199498
+    "redhat6"   = "RHEL-6.7_HVM_GA-*" 
+    "redhat8"   = "RHEL-8.2_HVM-*" 
+    # aws ec2 describe-images --owners 309956199498 --query 'sort_by(Images, &CreationDate)[*].[CreationDate,Name,ImageId]' --filters "Name=name,Values=RHEL-6*" --region eu-west-1 --output table
+    "windows2016" = "Windows_Server-2016-English-Full-Base*" #801119661308 
+    "windows2019" = "Windows_Server-2019-English-Full-Base*"
+    "ubuntu16"  = "ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server*" # 099720109477
+    "ubuntu18"  = "ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server*"
+    "ubuntu20"  = "ubuntu/images/hvm-ssd/ubuntu-groovy-20.10-amd64-server*"
+    # Ubuntu Image locator https://cloud-images.ubuntu.com/locator/ec2/
   }
 }
 
 variable "ami" {
-  default = "linux"  # awsami , windows, ubuntu
+  default = "awsami"  # awsami , windows, ubuntu
 }
 
 variable "kmsname" {
